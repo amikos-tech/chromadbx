dc0108ec58a669b24d30507eb895159aacc33c0e
Trayan Azarov <trayan.azarov@amikos.tech>
2024-09-10 17:49:22 +0300

    feat: make chroma package optional

--- chromadbx/__init__.py
+++ chromadbx/__init__.py
@@ -1,3 +1,11 @@
+try:
+    import chromadb  # noqa: F401
+except ImportError:
+    raise ValueError(
+        "The chromadb is not installed. This package (chromadbx) requires that Chroma is installed to work. "
+        "Please install it with `pip install chromadb`"
+    )
+
 from chromadbx.core.ids import (
     IDGenerator,
     NanoIDGenerator,
