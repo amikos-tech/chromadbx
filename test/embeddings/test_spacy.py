942a32191ddbcab3fde4245a3ff04da6b07370d7
Trayan Azarov <trayan.azarov@amikos.tech>
2025-01-15 17:54:58 +0200

    chore:  Fix linting errors

--- test/embeddings/test_spacy.py
+++ test/embeddings/test_spacy.py
@@ -1,21 +1,17 @@
-
-
 import pytest
 from chromadbx.embeddings.spacy import SpacyEmbeddingFunction
-spacy = pytest.importorskip("spacy", reason="spacy not installed")
 import subprocess
 
-def download_model(model_name: str):
+spacy = pytest.importorskip("spacy", reason="spacy not installed")
+
+
+def download_model(model_name: str) -> None:
     s = subprocess.run(["python", "-m", "spacy", "download", model_name])
     assert s.returncode == 0
 
-@pytest.fixture(scope="module")
-def model():
+
+def test_spacy() -> None:
     download_model("en_core_web_lg")
-    return "en_core_web_lg"
-
-
-def test_spacy():
     ef = SpacyEmbeddingFunction()
     texts = ["hello world", "goodbye world"] * 1000
     embeddings = ef(texts)
@@ -24,7 +20,8 @@ def test_spacy():
     assert len(embeddings[0]) == 300
     assert len(embeddings[1]) == 300
 
-def test_spacy_with_model():
+
+def test_spacy_with_model() -> None:
     download_model("en_core_web_sm")
     ef = SpacyEmbeddingFunction(model_name="en_core_web_sm")
     embeddings = ef(["hello world", "goodbye world"])
@@ -34,7 +31,7 @@ def test_spacy_with_model():
     assert len(embeddings[1]) == 96
 
 
-def test_spacy_with_invalid_model():
+def test_spacy_with_invalid_model() -> None:
     with pytest.raises(ValueError) as e:
-        ef = SpacyEmbeddingFunction(model_name="invalid_model")
+        SpacyEmbeddingFunction(model_name="invalid_model")
     assert "spacy model 'invalid_model' are not downloaded yet" in str(e.value)
